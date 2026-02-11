efb21e00fee1fdc71aed2356ea917862831112a4
sp41414 <134581084+sp41414@users.noreply.github.com>
2026-02-11 23:11:49 +0400

    chore: setup wails project react+ts

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+)
+
+var assets embed.FS
+
+func main() {
+	err := wails.Run(&options.App{
+		Title: "Chess",
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+	})
+
+	if err != nil {
+		println("error:", err.Error())
+	}
+}
